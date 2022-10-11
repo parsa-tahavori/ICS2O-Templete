b016596729f1015a0d1ec5035caef56d7bfb932e
parsa-tahavori <parsa-tahavori@users.noreply.github.com>
2022-10-11 18:12:28 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-function doMathClicked() { 
-document.getElementById("add-math").innerHTML = "<p>The area is= " + (5 * 6) + "</p>"
-document.getElementById("multiply-math").innerHTML = "<p>The perimeter is= " + (5 + 6 + 5 + 6) + "</p>" 
+function doMathClicked() {
+  document.getElementById("add-math").innerHTML =
+    "<p>The area is= " + 5 * 6 + "</p>"
+  document.getElementById("multiply-math").innerHTML =
+    "<p>The perimeter is= " + (5 + 6 + 5 + 6) + "</p>"
 }
