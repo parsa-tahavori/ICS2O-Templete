5ca20e49b69cbff451aa8932688b9bfaa1822aa3
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2022-10-11 18:10:14 +0000

    fixing the positions

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function doMathClicked() { 
+document.getElementById("add-math").innerHTML = "<p>The area is= " + (5 * 6) + "</p>"
+document.getElementById("multiply-math").innerHTML = "<p>The perimeter is= " + (5 + 6 + 5 + 6) + "</p>" 
+}
